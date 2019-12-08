63a06ff24c499b8f6c6a005bb9ce816fcedd0305
Терещенко Дмитрий <dima123-99@mail.ru>
2019-12-08 16:09:47 +0300

    Added the ratio of T(2n)/T(n)

--- empiricalAnalysis.py
+++ empiricalAnalysis.py
@@ -45,7 +45,7 @@ def _show_empirical_f(empirical_f):
     ax.legend()
 
     plt.show()
-    fig.savefig('empiricalAnalysis_data/empirical_f.png')
+    fig.savefig('empiricalAnalysis_data/empirical_f_test1.png')
 
 
 def empirical_analysis():
@@ -56,24 +56,24 @@ def empirical_analysis():
 
     '''Getting of empirical f'''
     f = [None] * (range_n[1] - range_n[0])
-    # for i, n in enumerate(range(*range_n)):
-    #     print(n)
-    #     f_on_n = [None] * m
-    #     for j in range(m):
-    #         Graph, start_node = Dijkstra.generate_data(n)
-    #         f_repeats = [0] * repeats
-    #         for l in range(repeats):
-    #             _, _, f_repeats[l] = Dijkstra.algorithm(Graph, start_node)
-    #         f_on_n[j] = sum(f_repeats) / repeats
-    #     f[i] = sum(f_on_n) / m
+    for i, n in enumerate(range(*range_n)):
+        print(n)
+        f_on_n = [None] * m
+        for j in range(m):
+            Graph, start_node = Dijkstra.generate_data(n)
+            f_repeats = [0] * repeats
+            for l in range(repeats):
+                _, _, f_repeats[l] = Dijkstra.algorithm(Graph, start_node)
+            f_on_n[j] = sum(f_repeats) / repeats
+        f[i] = sum(f_on_n) / m
 
     '''Up/down load data'''
     # with open('empiricalAnalysis_data/empirical_f_data.txt', 'w') as data:
     #     for f_i in f:
     #         data.write(f"{str(f_i)}\n")
-    with open('empiricalAnalysis_data/empirical_f_data.txt', 'r') as data:
-        for i in range(len(f)):
-            f[i] = float(data.readline())
+    # with open('empiricalAnalysis_data/empirical_f_data.txt', 'r') as data:
+    #     for i in range(len(f)):
+    #         f[i] = float(data.readline())
 
     _show_empirical_f(f)
 
@@ -81,10 +81,10 @@ def empirical_analysis():
     n = np.arange(*range_n)
     f = np.array(f)
     g = n ** 2
-    ratio = f / g
+    ratio_f_g = f / g
     for i in range(len(n)):
-        C1 = min(ratio[i:])
-        C2 = max(ratio[i:])
+        C1 = min(ratio_f_g[i:])
+        C2 = max(ratio_f_g[i:])
         if C1 > 0 and C2 > 0:
             print('n0:', n[i])
             print('C1:', C1)
@@ -94,16 +94,30 @@ def empirical_analysis():
     _show_empirical_f_with_asymptotic(f, C1, C2)
 
     '''Ratio: f(n)/n^2'''
-    # asymptotic
-    C = min(ratio)
+    #asymptotic
+    C = min(ratio_f_g)
     print('C', C)
-
+    
     fig, ax = plt.subplots()
     ax.set_xlabel("n")
-    ax.scatter(range(*range_n), ratio, marker='o', s=4, c="red", edgecolor='b')
-    ax.plot(range(*range_n), ratio, color="red", linestyle='--', label="$f(n)/n^2$")
+    ax.scatter(range(*range_n), ratio_f_g, marker='o', s=4, c="red", edgecolor='b')
+    ax.plot(range(*range_n), ratio_f_g, color="red", linestyle='--', label="$f(n)/n^2$")
     ax.plot(range(*range_n), [C] * (range_n[1] - range_n[0]), color="green", linestyle='--', label="C")
     ax.legend()
     plt.show()
     fig.savefig('empiricalAnalysis_data/ratio_f_g.png')
+
+    '''Ratio: f(2n)/f(n)'''
+    n = range(*range_n)[:range_n[1] // 2 - range_n[0] + 1]
+    f_n_1 = np.array(f[:range_n[1] // 2 - range_n[0] + 1])
+    f_n_2 = np.array(f[range_n[0] * 2 - range_n[0]::2])
+    ratio_f2_f1 = f_n_2 / f_n_1
+    fig, ax = plt.subplots()
+    ax.set_xlabel("n")
+    ax.scatter(n, ratio_f2_f1, marker='o', s=4, c="red", edgecolor='b')
+    ax.plot(n, ratio_f2_f1, color="red", linestyle='--', label="$f(2n)/f(n)$")
+    ax.legend()
+    plt.show()
+    fig.savefig('empiricalAnalysis_data/ratio_f2_f1.png')
+
 
